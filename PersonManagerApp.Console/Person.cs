80d6d9228f9500e3ebd2221f0c3d9f69c0b9db6f
Starinin Andrey <starininandrey@gmail.com>
2023-09-23 12:33:26 +0300

    Initial commit

---
+++ PersonManagerApp.Console/Person.cs
@@ -0,0 +1,22 @@
+﻿namespace PersonManagerApp.Console;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string FullName => $"{LastName} {FirstName}";
+    
+    public DateTime DateOfBirth { get; set; }
+
+    public int Age
+    {
+        get
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
---
+++ PersonManagerApp.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+using PersonManagerApp.Console;
+
+const string CONNECTION_STRING = @"Data Source=P:\PersonManagerApp\SQL\persons.db;";
+var db = new SqliteConnection(CONNECTION_STRING);
+db.Open();
+
+var sql = "SELECT * FROM table_persons";
+var command = new SqliteCommand(commandText: sql, connection: db);
+var result = command.ExecuteReader();
+
+var persons = new List<Person>();
+
+if (result.HasRows)
+{
+    while (result.Read())
+    {
+        persons.Add(new Person
+        {
+            Id = result.GetInt32("id"),
+            FirstName = result.GetString("first_name"),
+            LastName = result.GetString("last_name"),
+            DateOfBirth = result.GetDateTime("date_of_birth")
+        });
+    }
+}
+
+db.Close();
+
+foreach (var p in persons)
+{
+    Console.WriteLine($"{p.Id}: {p.FullName}, {p.Age}");
+}
